fef98dcb3ef2cfda28723e6a4cb1204dff25c5d7
nnedelko <121298101+nnedelko@users.noreply.github.com>
2024-11-18 18:08:08 +0300

    Update __init__.py

--- montepython/likelihoods/hst/__init__.py
+++ montepython/likelihoods/hst/__init__.py
@@ -7,8 +7,9 @@ class hst(Likelihood_prior):
     # initialisation of the class is done within the parent Likelihood_prior. For
     # this case, it does not differ, actually, from the __init__ method in
     # Likelihood class.
-    def loglkl(self, cosmo, data):
-
+    def loglkl_call(h0,h,sigma):
+        return -0.5 * (h0 - h) ** 2 / (sigma ** 2)
+    def loglkl_and_grad(self, cosmo, data):
         h = cosmo.h()
-        loglkl = -0.5 * (h - self.h) ** 2 / (self.sigma ** 2)
-        return loglkl
+        loglkl = value_and_grad(loglkl_call,[0,1,2]
+        return loglkl(cosmo.h,self.h,self.sigma)
